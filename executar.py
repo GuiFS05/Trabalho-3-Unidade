1f12765883a99d1bfa868b22699d4deb76b787b5
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2026-07-03 01:07:07 -0300

    tempo_insercao
    tempo para arvore e livre

--- executar.py
+++ executar.py
@@ -9,17 +9,27 @@ CAPACIDADE = 15000 # Capacidade definida
 tabela_com_arvore = HashTableArvore(CAPACIDADE)
 tabela_com_livre = HashTableArvore(CAPACIDADE)
 
+tempo_final_arvore = 0
+tempo_final_livre = 0
+
 # INSERCAO ARVORE
 print('Processando insercoes')
-inicio_arvore = time.perf_counter()
-
 with open('insercao.csv', mode='r') as arquivo:
     leitor = csv.reader(arquivo)
 
     for linha in leitor:
         cpf = int(linha[0])
         dados = linha[1:]
+        
+        inicio_arvore = time.perf_counter()
         tabela_com_arvore.tabela_inserir(cpf, dados)
+        fim_arvore = time.perf_counter()
+        tempo_final_arvore += (fim_arvore - inicio_arvore)
 
-fim_arvore = time.perf_counter()
-tempo_arvore = fim_arvore - inicio_arvore
+        inicio_livre = time.perf_counter()
+        tabela_com_livre.tabela_inserir(cpf, dados)
+        fim_livre = time.perf_counter()
+        tempo_final_livre += (fim_livre - inicio_livre)
+
+print(f"Tempo total de inserção na Árvore: {tempo_final_arvore:.6f} segundos")
+print(f"Tempo total de inserção no Enc. Livre: {tempo_final_livre:.6f} segundos")
